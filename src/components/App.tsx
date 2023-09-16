6252a59b8ff7c0b0cae09278d4f022851eed173d
famovkin <famovkin@yandex.ru>
2023-09-16 03:42:54 +0300

    day 5

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,22 @@
-import { Counter } from './Counter';
+import { Suspense } from 'react';
+import { Route, Routes, Link } from 'react-router-dom';
+import { AboutPageLazy } from '../pages/AboutPage/AboutPage.lazy';
+import { MainPageLazy } from '../pages/MainPage/MainPage.lazy';
 
 import './index.scss';
+import {} from 'react-router-dom';
 
 export const App = () => {
   return (
     <div className="app">
-      Title
-      <Counter />
+      <Link to={'/'}>Главная</Link>
+      <Link to={'/about'}>О нас</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={'/about'} element={<AboutPageLazy />} />
+          <Route path={'/'} element={<MainPageLazy />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
