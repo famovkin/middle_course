281a4b0621454ad642f0fa425547f20e282f1f27
famovkin <famovkin@yandex.ru>
2023-09-19 21:24:20 +0300

    day 8

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,7 @@ import { Suspense } from 'react';
 import { Route, Routes, Link } from 'react-router-dom';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from 'app/providers/ThemeProvider';
+import { AppRouter } from './providers/router';
 import { AboutPage } from 'pages/AboutPage';
 import { MainPage } from 'pages/MainPage';
 
@@ -15,12 +16,7 @@ export const App = () => {
       <button onClick={toggleTheme}>Тема</button>
       <Link to={'/'}>Главная</Link>
       <Link to={'/about'}>О нас</Link>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path={'/about'} element={<AboutPage />} />
-          <Route path={'/'} element={<MainPage />} />
-        </Routes>
-      </Suspense>
+      <AppRouter />
     </div>
   );
 };
