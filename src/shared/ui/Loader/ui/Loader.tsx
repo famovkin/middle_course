9d5909def428c71a4ca4f3127e9ff8ce01cd283a
famovkin <famovkin@yandex.ru>
2023-09-28 22:08:14 +0300

    day: 17

---
+++ src/shared/ui/Loader/ui/Loader.tsx
@@ -0,0 +1,14 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+
+import './Loader.scss';
+
+export const Loader = () => (
+  <div className={classNames('lds-ellipsis', {}, [])}>
+    <div />
+    <div />
+    <div />
+    <div />
+  </div>
+);
+
+export default Loader;
