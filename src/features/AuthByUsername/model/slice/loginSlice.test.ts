3f04468855d0826c2ed30b5fc25f9469e2e0d481
famovkin <famovkin@yandex.ru>
2023-11-11 19:59:32 +0300

    day 44

---
+++ src/features/AuthByUsername/model/slice/loginSlice.test.ts
@@ -0,0 +1,24 @@
+import { DeepPartial } from '@reduxjs/toolkit';
+import { LoginSchema } from '../types/loginSchema';
+import { loginActions, loginReducer } from './loginSlice';
+
+describe('loginSlice.test', () => {
+  test('test set username', () => {
+    const state: DeepPartial<LoginSchema> = { username: 'oldUsername' };
+    expect(
+      loginReducer(
+        state as LoginSchema,
+        loginActions.setUsername('newUsername'),
+      ),
+    ).toEqual({ username: 'newUsername' });
+  });
+  test('test set password', () => {
+    const state: DeepPartial<LoginSchema> = { username: 'oldPassword' };
+    expect(
+      loginReducer(
+        state as LoginSchema,
+        loginActions.setUsername('newPassword'),
+      ),
+    ).toEqual({ username: 'newPassword' });
+  });
+});
