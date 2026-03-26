9a3230f11a4761655e4d3a9b9c2f45528a417dfd
Tolya Fam <famovkin@yandex.ru>
2026-03-26 21:18:31 +0300

    6: add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import { Counter } from './components/Counter';
+import { Suspense } from 'react';
+import { Link, Route, Routes } from 'react-router-dom';
+
+import { AboutPageLazy } from './pages/AboutPage/AboutPage.async';
+import { MainPageLazy } from './pages/MainPage/MainPage.async';
 
 import './index.scss';
 
 const App = () => {
   return (
     <div className="app">
-      <Counter />
+      <Link to="/about">About</Link>
+      <br />
+      <Link to="/">Main</Link>
+      <Suspense fallback={<h1>Loading...</h1>}>
+        <Routes>
+          <Route path="/about" element={<AboutPageLazy />} />
+          <Route path="/" element={<MainPageLazy />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
