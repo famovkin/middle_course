38293164f9edca24b37efc71a952ac52670147b9
Tolya Fam <famovkin@yandex.ru>
2026-03-26 22:28:03 +0300

    7: add themes

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,16 @@ import { Link, Route, Routes } from 'react-router-dom';
 
 import { AboutPageLazy } from './pages/AboutPage/AboutPage.async';
 import { MainPageLazy } from './pages/MainPage/MainPage.async';
+import { useTheme } from './theme/useTheme';
 
-import './index.scss';
+import './styles/index.scss';
 
 const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>Change theme</button>
       <Link to="/about">About</Link>
       <br />
       <Link to="/">Main</Link>
